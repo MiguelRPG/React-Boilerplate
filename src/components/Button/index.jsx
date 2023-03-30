771f289871d384fbe0ed23fadc4c72daacf7429f
MiguelRPG <miguel.soares.tech@gmail.com>
2023-03-29 21:41:26 -0300

    refactor(button): update button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './style.css';
+import "./style.css";
 
 const Button = (props) => {
   return (
-    <button
-      className={`button ${props.theme}`}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </button>
+    <div className="inputs">
+
+      <button
+        name={props.name}
+        id={props.id}
+        className={`button ${props.status ? props.status : ""} ${props.className ? props.className : ""}`}
+        onClick={props.onClick}
+        disabled={props.disabled}
+        aria-describedby={props.errorMsg ? `${props.id}-error` : undefined}
+      >
+        {props.children}
+      </button>
+
+      {props.errorMsg && (
+        <div
+          id={`${props.id}-error`}
+          className={props.status ? props.status : ""}
+          role="alert"
+        >
+          {props.errorMsg}
+        </div>
+      )}
+    </div>
   );
 };
 
